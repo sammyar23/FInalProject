e461514235f58616272b40c0c68ff265b1894220
sammy <sammyarora23@gmail.com>
2024-01-12 08:57:19 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,21 @@ const fs = require('fs').promises;
 // Define the port variable
 const port = process.env.PORT || 3000; // Use the provided port or default to 3000
 
+
+
 // Set Pug as the view engine
 app.set('view engine', 'pug');
 
+
 // Note: When running on Azure, __dirname will be /home/site/wwwroot if app.js is in the root
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/', (req, res) => res.render('home'));
+app.get('/home', (req, res) => res.render('home'));
+
 
 app.get('/api/components/cpu', async (req, res) => {
     try {
@@ -70,20 +80,5 @@ app.get('/api/components/cpu', async (req, res) => {
     res.send('Build submitted successfully!'); // Or res.redirect('/some-page');
   });
   
-//app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-  
-  
-  
-
-
-// Serve static files from the 'public' directory
-// Ensure the 'public' directory is also within /home/site/wwwroot
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Define routes for your application
-app.get('/', (req, res) => res.render('home'));
-app.get('/home', (req, res) => res.render('home'));
-app.get('/build-pc', (req, res) => res.render('build-pc'));
-
 // Start the server on the defined port
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
