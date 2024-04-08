f24529ceb4a53166141b6e0b6639f9a3862fd0f3
sammy <sammyarora23@gmail.com>
2024-04-08 22:26:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -44,9 +44,15 @@ passport.use(new LocalStrategy(
   }
 ));
 passport.serializeUser((user, done) => done(null, user.id));
-passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => done(err, user));
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
 });
+
 
 // Initialize passport
 app.use(passport.initialize());
